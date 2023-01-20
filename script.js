eb65aa0ec907e5cc06acc119cb293ce737331829
Daniel Morris <daniel.morris46@hotmail.com>
2023-01-19 19:36:30 -0600

    Initial navigation menu

---
+++ script.js
@@ -0,0 +1,39 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+function showDropdown(dropdown) {
+    $('#' + dropdown).toggle();
+}
+
+function hideDropdowns() {
+    $(".dropdown-content").each(function() {
+        $(this).hide();
+    });
+}
+
+$(window).click(function(e) {
+    if (e.target.matches('.nav-link') || (
+        e.target.parentElement && 
+        e.target.parentElement.matches('.dropdown-content')
+    )) {
+      console.log('matched');
+    } else {
+        hideDropdowns();
+    }
+  });
